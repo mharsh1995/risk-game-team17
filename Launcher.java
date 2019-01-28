afc730b2186ea89812ae79fd2142fe61fb86ee14
mharsh1995 <47004894+mharsh1995@users.noreply.github.com>
2019-01-27 20:01:25 -0500

    Add files via upload

--- Launcher.java
+++ Launcher.java
@@ -15,16 +15,16 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 import Risk.Game.Team.View.Launcher;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import java.awt.GridBagLayout;
+//import java.awt.GridBagConstraints;
+//import java.awt.Insets;
 
 /* This class represent Menu to start and Exit game */
 
 public class Launcher extends JFrame implements ActionListener {
-	private static Logger LOGGER = LogManager.getLogger();
+	//private static Logger LOGGER = LogManager.getLogger();
 	private JButton StartGameButton;
 	private JButton EndGameButton;
 	private JButton CreateMapButton;
@@ -40,38 +40,38 @@ public class Launcher extends JFrame implements ActionListener {
 		}
 	catch(Exception ex)
 	{
-		LOGGER.error("Error" + ex.getMessage());
-			
+		//LOGGER.error("Error" + ex.getMessage());
+		System.out.println ("error loading");	
 	}
 	}
 	
 	public void Strt() {
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		GameFrame = new JFrame("Concordia Conquest");
+		GameFrame = new JFrame("Risk Game");
 		GameFrame.setVisible(true);
 
 		GameFrame.setSize(500, 500);
 		GameFrame.setLocation(500, 200);
 		GameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		border = new TitledBorder("Concordia Conquest");
+		border = new TitledBorder("Risk Game");
 		border.setTitleJustification(TitledBorder.CENTER);
 		border.setTitlePosition(TitledBorder.TOP);
 
 		GamePanel = new JPanel();
 		GamePanel.setBorder(border);
 
-		TitleLabel = new JLabel("Concordia Conquest");
+		TitleLabel = new JLabel("Risk Game");
 
-		TitleLabel.setForeground(Color.black);
-		TitleLabel.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+		TitleLabel.setForeground(Color.red);
+		TitleLabel.setFont(new Font("TimesRoman", Font.BOLD, 14));
 
 		GameFrame.getContentPane().add(TitleLabel);
 		GameFrame.getContentPane().add(GamePanel);
 		GamePanel.setLayout(null);
 		StartGameButton = new JButton("Start Game");
 		GamePanel.add(StartGameButton);
-		StartGameButton.setFont(new Font("Georgia", Font.PLAIN, 10));
+		StartGameButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
 		StartGameButton.setBackground(UIManager.getColor("Button.highlight"));
 		StartGameButton.setForeground(new Color(102, 0, 0));
 		StartGameButton.setForeground(new Color(102, 0, 0));
@@ -81,13 +81,13 @@ public class Launcher extends JFrame implements ActionListener {
 		CreateMapButton = new JButton("Create Map");
 		GamePanel.add(CreateMapButton);
 		CreateMapButton.setForeground(new Color(102, 0, 0));
-		CreateMapButton.setFont(new Font("Georgia", Font.PLAIN, 10));
+		CreateMapButton.setFont(new Font("TimesRomen", Font.BOLD, 15));
 
 		CreateMapButton.setBounds(186, 158, 121, 21);
 		CreateMapButton.addActionListener(this);
 		EditMapButton = new JButton("Edit map");
 		GamePanel.add(EditMapButton);
-		EditMapButton.setFont(new Font("Georgia", Font.PLAIN, 10));
+		EditMapButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
 		EditMapButton.setForeground(new Color(102, 0, 0));
 		EditMapButton.setBackground(UIManager.getColor("Button.highlight"));
 
@@ -97,7 +97,7 @@ public class Launcher extends JFrame implements ActionListener {
 		EndGameButton = new JButton("Exit");
 		GamePanel.add(EndGameButton);
 		EndGameButton.setForeground(new Color(102, 0, 0));
-		EndGameButton.setFont(new Font("Georgia", Font.PLAIN, 10));
+		EndGameButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
 
 		EndGameButton.setBounds(186, 371, 121, 21);
 		EndGameButton.addActionListener(this);
