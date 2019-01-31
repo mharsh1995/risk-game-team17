9ab6900264bb149eab19ad4e536c48ec2b4548ee
Yash Golwala <golwalayash@gmail.com>
2019-01-31 00:22:32 -0500

    Launcher with main screen
    start game button
    end game button
    edit  button
    create button

--- Launcher.java
+++ Launcher.java
@@ -1,34 +1,32 @@
-package Risk.Game.Team.View;
-
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
-import Risk.Game.Team.View.Launcher;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 /* This class represent Menu to start and Exit game */
 
 public class Launcher extends JFrame implements ActionListener {
 
-	private JButton StartGameButton;
-	private JButton EndGameButton;
-	private JButton CreateMapButton;
-	private JButton EditMapButton;
-	private JButton SeriesButton;
-	private JFrame GameFrame;
+	private static final long serialVersionUID = 1L;
+	private JButton StartBtn;
+	private JButton EndBtn;
+	private JButton CreateBtn;
+	private JButton EditBtn;
+	private JButton SeriesBtn;
+	private JFrame WindowFrame;
 	private TitledBorder border;
-	private JPanel GamePanel;
+	private JPanel WindowPanel;
 	private JLabel TitleLabel;
 
 	public Launcher () {
@@ -46,60 +44,59 @@ public class Launcher extends JFrame implements ActionListener {
 
 	public void Strt() {
 
-		GameFrame = new JFrame("Risk Game");
-		GameFrame.setVisible(true);
+		WindowFrame = new JFrame("Risk-Play!");
+		WindowFrame.setVisible(true);
 
-		GameFrame.setSize(500, 500);
-		GameFrame.setLocation(500, 200);
-		GameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		WindowFrame.setSize(550, 550);
+		WindowFrame.setLocation(500, 200);
+		WindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		border = new TitledBorder("Risk Game");
+		border = new TitledBorder("Risk - Ready for War");
 		border.setTitleJustification(TitledBorder.CENTER);
 		border.setTitlePosition(TitledBorder.TOP);
 
-		GamePanel = new JPanel();
-		GamePanel.setBorder(border);
+		WindowPanel = new JPanel();
+		WindowPanel.setBorder(border);
 
-		TitleLabel = new JLabel("Risk Game");
+		TitleLabel = new JLabel("Risk");
 
 		TitleLabel.setForeground(Color.red);
-		TitleLabel.setFont(new Font("TimesRoman", Font.BOLD, 14));
+		TitleLabel.setFont(new Font("Monospaced", Font.BOLD, 14));
 
-		GameFrame.getContentPane().add(TitleLabel);
-		GameFrame.getContentPane().add(GamePanel);
-		GamePanel.setLayout(null);
-		StartGameButton = new JButton("Start Game");
-		GamePanel.add(StartGameButton);
-		StartGameButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
-		StartGameButton.setBackground(UIManager.getColor("Button.highlight"));
-		StartGameButton.setForeground(new Color(102, 0, 0));
-		StartGameButton.setForeground(new Color(102, 0, 0));
+		WindowFrame.getContentPane().add(TitleLabel);
+		WindowFrame.getContentPane().add(WindowPanel);
+		WindowPanel.setLayout(null);
+		StartBtn = new JButton("Start Game");
+		WindowPanel.add(StartBtn);
+		StartBtn.setFont(new Font("Monospaced", Font.BOLD, 15));
+		StartBtn.setBackground(UIManager.getColor("Button.highlight"));
+		StartBtn.setForeground(new Color(67, 80, 88));
 
-		StartGameButton.setBounds(186, 102, 121, 21);
-		StartGameButton.addActionListener(this);
-		CreateMapButton = new JButton("Create Map");
-		GamePanel.add(CreateMapButton);
-		CreateMapButton.setForeground(new Color(102, 0, 0));
-		CreateMapButton.setFont(new Font("TimesRomen", Font.BOLD, 15));
+		StartBtn.setBounds(220, 102, 125, 21);
+		StartBtn.addActionListener(this);
+		CreateBtn = new JButton("Create Map");
+		WindowPanel.add(CreateBtn);
+		CreateBtn.setForeground(new Color(67, 80, 88));
+		CreateBtn.setFont(new Font("Monospaced", Font.BOLD, 15));
 
-		CreateMapButton.setBounds(186, 158, 121, 21);
-		CreateMapButton.addActionListener(this);
-		EditMapButton = new JButton("Edit map");
-		GamePanel.add(EditMapButton);
-		EditMapButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
-		EditMapButton.setForeground(new Color(102, 0, 0));
-		EditMapButton.setBackground(UIManager.getColor("Button.highlight"));
+		CreateBtn.setBounds(220, 178, 125, 21);
+		CreateBtn.addActionListener(this);
+		EditBtn = new JButton("Edit map");
+		WindowPanel.add(EditBtn);
+		EditBtn.setFont(new Font("Monospaced", Font.BOLD, 15));
+		EditBtn.setForeground(new Color(67, 80, 88));
+		EditBtn.setBackground(UIManager.getColor("Button.highlight"));
+								
+		EditBtn.setBounds(220, 214, 125, 21);
+		EditBtn.addActionListener(this);
 
-		EditMapButton.setBounds(186, 214, 121, 21);
-		EditMapButton.addActionListener(this);
+		EndBtn = new JButton("Exit");
+		WindowPanel.add(EndBtn);
+		EndBtn.setForeground(new Color(67, 80, 88));
+		EndBtn.setFont(new Font("Monospaced", Font.BOLD, 15));
 
-		EndGameButton = new JButton("Exit");
-		GamePanel.add(EndGameButton);
-		EndGameButton.setForeground(new Color(102, 0, 0));
-		EndGameButton.setFont(new Font("TimesRoman", Font.BOLD, 15));
-
-		EndGameButton.setBounds(186, 371, 121, 21);
-		EndGameButton.addActionListener(this);
+		EndBtn.setBounds(220, 371, 125, 21);
+		EndBtn.addActionListener(this);
 
 	}
 	public static void main(String[] args) {
