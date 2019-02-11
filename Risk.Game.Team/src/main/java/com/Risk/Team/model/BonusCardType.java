6250ed6852ccfa2dfc9c1cee2e0165dc86b79a4c
Yash Golwala <golwalayash@gmail.com>
2019-02-11 00:56:54 -0500

    Bonuscardtype file created

---
+++ Risk.Game.Team/src/main/java/com/Risk/Team/model/BonusCardType.java
@@ -0,0 +1,16 @@
+package com.Risk.Team.model;
+
+/**
+ * Interface to define the type of Cards
+ * 
+ * @author yashGolwala
+ *
+ */
+
+public interface BonusCardType {
+	String infantry = "infantry";
+	String artillery = "artillery";
+	String cavalry ="cavalry";
+
+}
+
