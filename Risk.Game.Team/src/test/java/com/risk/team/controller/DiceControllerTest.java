80ec07bdb50971ed3824f0275c2fd4f4ad36a49a
Harsh Mehta <harshmehta493@gmail.com>
2019-03-26 21:39:18 -0400

    Test case of DiceControllerTest

---
+++ Risk.Game.Team/src/test/java/com/risk/team/controller/DiceControllerTest.java
@@ -0,0 +1,14 @@
+package com.risk.team.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class DiceControllerTest {
+	private RiskDiceController dice;
+
+	
+}
