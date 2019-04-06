ed2a987dc254eca359830d2e962241bcdc846fe9
Harsh Mehta <harshmehta493@gmail.com>
2019-04-06 02:29:33 -0400

    PlayerMode model file created.

---
+++ Risk.Game.Team/src/main/java/com/risk/team/model/PlayerMode.java
@@ -0,0 +1,16 @@
+package com.risk.team.model;
+
+/**
+ * Interface which defines the types of PLAYER.
+ * 
+ * @author yashgolwala
+ * @author harsh mehta
+ *
+ */
+public interface PlayerMode {
+	String HUMAN = "Human";
+	String AGGRESSIVE = "Aggressive";
+	String BENEVOLENT = "Benevolent";
+	String RANDOM = "Random";
+	String CHEATER = "Cheater";
+}
